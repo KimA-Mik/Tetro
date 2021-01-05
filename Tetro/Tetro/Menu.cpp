3b0bb4035d8659b88fbd79773348292431a7b18a
KimA-Mik <ananim38@gmail.com>
2021-01-06 02:09:50 +0300

    Complete menu graphics

--- Tetro/Tetro/Menu.cpp
+++ Tetro/Tetro/Menu.cpp
@@ -6,6 +6,19 @@ Menu::Menu(SDL_Renderer* Renderer, bool* PorgramStatus)
 	mRenderer = Renderer;
 	IsRunning = PorgramStatus;
 	Blocks.loadFromFile(mRenderer, "Assets/Images/Tetro.png");
+	Logo.loadFromFile(mRenderer, "Assets/Images/Logo.png");
+	OnePButton.loadFromFile(mRenderer, "Assets/Images/OnePButton.png");
+	TwoPButton.loadFromFile(mRenderer, "Assets/Images/TwoPButton.png");
+	StartGame.loadFromFile(mRenderer, "Assets/Images/StartGame.png");
+	ExitGame.loadFromFile(mRenderer, "Assets/Images/ExitGame.png");
+
+	int TempButtonDist = 50;
+
+	OPButton = { TempButtonDist,200,OnePButton.getWidth(),OnePButton.getHeight() / 2 };
+	TPButton = { 1280 - TempButtonDist - TwoPButton.getWidth(),200,TwoPButton.getWidth(),TwoPButton.getHeight() / 2 };
+
+	SGButton = { 1280 / 2 - StartGame.getWidth() / 2,400,StartGame.getWidth(),StartGame.getHeight() };
+	EGButton = { 1280 / 2 - ExitGame.getWidth() / 2,600,ExitGame.getWidth(),ExitGame.getHeight() };
 
 	for (int x = 0; x < 40; x++)
 		for (int y = 0; y < 23; y++)
@@ -15,7 +28,11 @@ Menu::Menu(SDL_Renderer* Renderer, bool* PorgramStatus)
 Menu::~Menu()
 {
 	Blocks.free();
-	//Background.free();
+	Logo.free();
+	OnePButton.free();
+	TwoPButton.free();
+	StartGame.free();
+	ExitGame.free();
 }
 
 int Menu::Run()
@@ -39,16 +56,37 @@ int Menu::Run()
 					aBackgroundField[x][y] = rand() % 7;
 		}
 
-
+		//рисуем фон
 		for (int x = 0; x < 40; x++)
 			for (int y = 0; y < 23; y++) {
 				SDL_Rect Clip = { aBackgroundField[x][y] * 32,0,32,32 };
 				Blocks.render(x * 32, y * 32, &Clip);
 			}
+
+
+		//рисуем кнопки 
+		//todo работа с кнопками
+		Logo.render(1280 / 2 - Logo.getWidth() / 2, 75);
+		SDL_Rect ButtonClip = { 0,0,OnePButton.getWidth(),OnePButton.getHeight() / 2 };
+		OnePButton.render(OPButton.x, OPButton.y, &ButtonClip);
+		TwoPButton.render(TPButton.x, TPButton.y, &ButtonClip);
+
+		SDL_Rect SGClip = { 0,75, StartGame.getWidth(), StartGame.getHeight() / 2 };
+		StartGame.render(SGButton.x, SGButton.y, &SGClip);
+
+		SDL_Rect EGClip = { 0,0,ExitGame.getWidth(),ExitGame.getHeight() / 2 };
+		ExitGame.render(EGButton.x, EGButton.y, &EGClip);
+
 		SDL_RenderPresent(mRenderer);
 		SDL_Delay(16);
 	}
 
 
 	return 0;
+}
+
+bool Menu::IsPointInRect(int x, int y, SDL_Rect &TarRect)
+{
+	return (x > TarRect.x && x < TarRect.x + TarRect.w
+		&& y > TarRect.y && y < TarRect.y + TarRect.h);
 }
--- Tetro/Tetro/Menu.h
+++ Tetro/Tetro/Menu.h
@@ -12,13 +12,36 @@ public:
 	Menu(SDL_Renderer* Renderer, bool* PorgramStatus);
 	~Menu();
 	int Run();
+
+	bool IsPointInRect(int x, int y, SDL_Rect &TarRect);
 private:
+	//стандартные блоки, складываютч в мозайку
 	LTexture Blocks;
+
+	//логотоип, всегда по центру
+	LTexture Logo;
+
+	//размер кнопок 200 на 200
+	//изображение 200 на 400
+	LTexture OnePButton;
+	LTexture TwoPButton;
+
+	//410 на 75
+	LTexture StartGame;
+
+	//480 на 75
+	LTexture ExitGame;
+
+	//четырехугольники с размерами кнопок
+	SDL_Rect OPButton;
+	SDL_Rect TPButton;
+	SDL_Rect SGButton;
+	SDL_Rect EGButton;
+
 	std::array<std::array<int, 23>, 40> aBackgroundField;
 
 	bool* IsRunning = nullptr;
 
 	SDL_Renderer* mRenderer = nullptr;
-	//LTexture Background;
 };
 //UTF-8 Коммент
